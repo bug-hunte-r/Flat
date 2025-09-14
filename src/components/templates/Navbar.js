baec47cc96d726497c3d1055a6a5cd2f4ca51d79
shayan <shayanjafarnejad75@gmail.com>
2025-09-14 12:08:56 +0330

    make responsive the navbar

--- src/components/templates/Navbar.js
+++ src/components/templates/Navbar.js
@@ -1,13 +1,23 @@
+"use client"
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import style from '../../../styles/navbar/nav.css'
+import media from '../../../styles/navbar/mediaNav.css'
+import { IoIosMenu } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
 
 function Navbar() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
     <div className='container-navbar'>
 
-      <div className='right-sec-navbar'>
+      <IoIosMenu className='icon-hamberger-menu' onClick={() => setIsMenuOpen(true)} />
+
+      <div className={`right-sec-navbar ${isMenuOpen ? 'open-menu' : 'close-menu'}`}>
+      <IoClose className='icon-close-menu' onClick={() => setIsMenuOpen(false)} />
       <Image src={'/imgs/logo.png'} width={600} height={600} alt='logo' className='logo'/>
       <Link href={'/'} className='nav-links'>فلت کارت</Link>
       <Link href={'/'} className='nav-links'>فروشگاه</Link>
