e8e8e5cfd90fbe3f322311c1bb40799c323ab88b
shayan <shayanjafarnejad75@gmail.com>
2025-09-16 13:53:47 +0330

    develop question section

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Header from "@/components/templates/Header";
 import Navbar from "../components/templates/Navbar";
 import Feature from "@/components/templates/Feature";
 import Pro from "@/components/templates/Pro";
+import Question from "@/components/templates/Question";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <Header />
         <Feature />
         <Pro />
+        <Question />
       </div>
     </>
   );
