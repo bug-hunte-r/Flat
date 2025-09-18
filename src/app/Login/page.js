3160a598fbf3f6b69d3e5378da479dcc5df9edc1
shayan <shayanjafarnejad75@gmail.com>
2025-09-18 08:56:36 +0330

    set style for login form

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -6,12 +6,14 @@ import Image from 'next/image'
 function Login() {
   return (
     <div className='sub-container'>
-      <div className='container-login-form'>
-        <Image src={'/imgs/logo.png'} width={100} height={100} alt='logo' />
-        <h4 className='title-login-form'>ورود</h4>
-        <p>سلام! <br /> لطفاً شماره موبایل خود را وارد کنید.</p>
-        <input type='number' placeholder='09' className='input-login-form' />
-        <button className='btn-login-form'>ورود</button>
+      <div className='sec-container'>
+        <div className='container-login-form'>
+          <Image src={'/imgs/logo.png'} width={100} height={100} alt='logo' className='logo-login-form' />
+          <h4 className='title-login-form'>ورود</h4>
+          <p className='text-login-form'>سلام! <br /> لطفاً شماره موبایل خود را وارد کنید.</p>
+          <input type='number' placeholder='09' className='input-login-form' />
+          <button className='btn-login-form'>ورود</button>
+        </div>
       </div>
     </div>
   )
