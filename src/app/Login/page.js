117ed8ee3792a8c4b5ce83a025a50d76da29decc
shayan <shayanjafarnejad75@gmail.com>
2025-09-18 11:49:22 +0330

    set timer for forms

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import style from '../../../styles/login/login.css'
 import media from '../../../styles/login/mediaLogin.css'
 import Image from 'next/image'
@@ -9,9 +9,23 @@ function Login() {
 
   const [isShowVerifyForm, setIsShowVerifyForm] = useState(false)
   const [isShowLoginForm, setIsShowLoginForm] = useState(true)
-
   const [phone, setPhone] = useState()
   const [code, setCode] = useState()
+  const [timeLeft, setTimeLeft] = useState(120);
+
+  useEffect(() => {
+    if (timeLeft <= 0) return;
+
+    const interval = setInterval(() => {
+      setTimeLeft((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [timeLeft]);
+
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
+
 
   return (
     <div className='sub-container'>
@@ -22,7 +36,7 @@ function Login() {
           <h4 className='title-login-form'>ورود</h4>
           <p className='text-login-form'>سلام! <br /> لطفاً شماره موبایل خود را وارد کنید.</p>
           <input type='number' placeholder='09' className='input-login-form' value={phone} onChange={event => setPhone(event.target.value)} />
-          <button className='btn-login-form' onClick={() => { setIsShowVerifyForm(true); setIsShowLoginForm(false) }}>ورود</button>
+          <button className='btn-login-form' onClick={() => { setIsShowVerifyForm(true); setIsShowLoginForm(false); setTimeLeft(120) }}>ورود</button>
         </div>
       </div>
 
@@ -35,8 +49,9 @@ function Login() {
           <h4 className='title-login-form'>کد تایید را وارد کنید</h4>
           <p className='text-login-form'>کد تایید برای شماره ۰۹۱۴۴۳۴۸۵۵۴ پیامک شد</p>
           <input type='number' className='input-login-form' value={code} onChange={event => setCode(event.target.value)} />
-          <p className='timer'>2:00</p>
-          <button className='btn-login-form'>تایید</button>
+          <p className='timer'>{minutes}:{seconds < 10 ? `0${seconds}` : seconds} مانده تا دریافت مجدد کد</p>
+          <button className={`btn-login-form ${timeLeft > 0 ? 'show-ok-btn' : 'Unshow-ok-btn'}`}>تایید</button>
+          <button className={`btn-login-form ${timeLeft === 0 ? 'show-resend-btn' : 'Unshow-resend-btn'}`} onClick={() => setTimeLeft(120)}>دریافت مجدد کد</button>
         </div>
       </div>
 
