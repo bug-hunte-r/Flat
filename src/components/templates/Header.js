2dde34ddf5f13d6d26a932ef9af1cc1101c6b747
shayan <shayanjafarnejad75@gmail.com>
2025-09-21 11:56:01 +0330

    develop logic for headers input

--- src/components/templates/Header.js
+++ src/components/templates/Header.js
@@ -1,9 +1,27 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import style from '../../../styles/header/header.css'
 import media from '../../../styles/header/mediaHeader.css'
 import Image from 'next/image'
 
 function Header() {
+
+    const [flatInput, setFlatInput] = useState('flatcard.net/')
+
+    const handleChange = (e) => {
+        let value = e.target.value;
+
+        if (!value.startsWith("flatcard.net/")) {
+            value = "flatcard.net/";
+        }
+
+        setFlatInput(value);
+    };
+
+    const searchLink = () => {
+        console.log(flatInput);
+    }
+
     return (
         <div className='container'>
             <div className='sub-container'>
@@ -13,8 +31,8 @@ function Header() {
                         <h1 className='title-header'>همین الان پروفایل خودتو بساز!</h1>
                         <p className='sub-title-header'>رایگانه و کمتر از 2 دقیقه طول میکشه</p>
                         <div className='container-btn-and-input-header'>
-                            <button className='btn-header'>بزن بریم</button>
-                            <input type='text' placeholder='flatcard.net/' className='input-header' />
+                            <button className='btn-header' onClick={searchLink}>بزن بریم</button>
+                            <input type='text' value={flatInput} onChange={handleChange} className='input-header' />
                         </div>
                     </div>
                     <Image src={'/imgs/header-img.png'} width={600} height={600} alt='header-img' className='imgs-header' />
